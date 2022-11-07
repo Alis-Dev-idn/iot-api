d8ecaa9d84e0d34d2d2227120dae1b352d8c8db6
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-07 14:50:22 +0700

    add wrong url

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,9 @@ const Routes = express();
 
 Routes.use("/user", UserRoute);
 
+/* Url wrong */
+Routes.use(function(req, res, next) {
+    return res.status(404).json({message: "wrong url"});
+});
+
 export default Routes;

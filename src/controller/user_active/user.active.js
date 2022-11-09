07b82bb7f2f31495bd5d71ccf36db379dc3adc72
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-09 15:23:09 +0700

    solve type

--- src/controller/user_active/user.active.js
+++ src/controller/user_active/user.active.js
@@ -8,7 +8,7 @@ const Active = async (req, res) => {
         if(!cekUser) return res.status(400).json({message: "user not found"});
         if(code !== cekUser.session.code) return res.status(400).json({message: "code wrong"});
         if(cekUser.session.active) return res.status(400).json({message: "user is active!"});
-        cekUser.session.active = true; 
+        cekUser.session.status = true;
         await UserService.UpdateUser("user", cekUser);
         res.status(200).json({status: "success", message: `account ${cekUser.username} is active now`});
     }catch(err){

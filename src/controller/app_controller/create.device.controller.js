3597d1ff838b69b1510139788e12f73fed6b7093
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-25 16:10:58 +0700

    solve error

--- src/controller/app_controller/create.device.controller.js
+++ src/controller/app_controller/create.device.controller.js
@@ -9,6 +9,8 @@ const CreateDevice = async (req, res) => {
         if(error) return res.status(400).json({message: error.details[0].message});
         if(!Validate.DbValidate(id)) return res.status(400).json({message: "Id not Valid"});
 
+        if(data.name === "testing") return res.status(400).json({message: "name not allowed"});
+
         const cekApp = await AppService.GetApp("id", id);
         const cekNameApp = cekApp.application.find(items => items === data.application);
         if(!cekNameApp) return res.status(400).json({message: "Application Not Found"});

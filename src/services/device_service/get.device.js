cbdad2f49bf46625e3766d0a25dc4a2b7ed90e78
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-25 14:17:24 +0700

    add CreateAt data

--- src/services/device_service/get.device.js
+++ src/services/device_service/get.device.js
@@ -4,7 +4,7 @@ import {dbDevice} from "./device.schema.js";
 const GetDevice = async (name, limit, skip) => {
     const db = await dbDevice(name);
     const count = await db.find({name: name}).count();
-    const data = await db.find({name: name}, {_id: 0, __v: 0, createdAt: 0, updatedAt: 0}).sort({createdAt: -1}).limit(limit).skip(skip);
+    const data = await db.find({name: name}, {_id: 0, __v: 0, updatedAt: 0}).sort({createdAt: -1}).limit(limit).skip(skip);
     return {count, data: data};
 }
 

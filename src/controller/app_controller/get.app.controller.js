48240313bcc566490279baf4033106bf6db117a6
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-29 09:50:08 +0700

    resolve error

--- src/controller/app_controller/get.app.controller.js
+++ src/controller/app_controller/get.app.controller.js
@@ -5,6 +5,7 @@ const GetAppController = async (req, res) => {
     try{
         const id = req._id;
         const getApp = await AppService.GetApp("id", id);
+        if(!getApp) return res.status(200).json({data: []});
         res.status(200).json({
             data: getApp.application
         });
--- src/controller/device_controller/get.count.device.js
+++ src/controller/device_controller/get.count.device.js
@@ -5,6 +5,7 @@ const getCountDevice = async (req, res) => {
     try{
         const id = req._id;
         const getApp = await AppService.GetApp("id", id);
+        if(!getApp) return res.status(200).json({count:0});
         const getCount = getApp.device.length;
         res.status(200).json({count: getCount});
     }catch (error){

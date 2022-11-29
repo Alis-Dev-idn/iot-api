b10c872b6f6ae399cf970616f80e49e1cfc54b8f
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-29 14:19:17 +0700

    add connect device

--- src/socket.io/listen_socket/listen.socket.js
+++ src/socket.io/listen_socket/listen.socket.js
@@ -1,24 +1,36 @@
 import SaveDataSocket from "../save_socket/save.data.socket.js";
 
+let connect = [];
+
 const ListenConnection = (io) => {
     io.on("connection", socket => {
         console.log("user connected : " + socket.id);
         socket.on("disconnect", () => {
             console.log("user disconnect : " + socket.id);
+            connect = connect.filter(items => items.id !== socket.id);
         });
         listenMessageNode(socket);
+        sendOnlineDevice(socket);
     });
 }
 
 const listenMessageNode = (socket) => {
     socket.on("send_data", async (data) => {
         try{
+            const online = connect.find(items => items.id === socket.id);
+            if(!online) connect.push({id: socket.id, key: data.key});
             const status = await SaveDataSocket(data);
             socket.emit(data.key, status);
             socket.broadcast.emit(data.key, {...data, createdAt: Math.floor(new Date())});
         }catch (err){
             socket.emit(data.key, {error: err});
         }
+    });
+}
+
+const sendOnlineDevice = (socket) => {
+    socket.on("get_online", async () => {
+        socket.emit("send_online", connect);
     });
 }
 

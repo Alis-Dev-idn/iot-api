51a0a7417e7a3f6123717c7f0c936fda01187ac6
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-29 09:12:48 +0700

    remove _id from sub-document object

--- src/mongodb/model/device.schema.js
+++ src/mongodb/model/device.schema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const DeviceSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    data: {type: {_id: false}, required: true}
+    data: {type: Object(), required: true}
 }, {timestamps: true});
 
 export default DeviceSchema;

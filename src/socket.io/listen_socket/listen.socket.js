624054150e0fb217090bd6382b909ac2dd79bb47
Alis-Dev-idn <ali.dev313@gmail.com>
2022-11-30 12:27:41 +0700

    add listen command from user

--- src/socket.io/listen_socket/listen.socket.js
+++ src/socket.io/listen_socket/listen.socket.js
@@ -21,7 +21,7 @@ const listenMessageNode = (socket) => {
             const online = connect.find(items => items.id === socket.id);
             if(!online) connect.push({id: socket.id, key: data.key});
             const status = await SaveDataSocket(data);
-            socket.broadcast.emit(data.key.split("-")[1], status);
+            socket.emit(data.key, status);
             socket.broadcast.emit(data.key, {...data, createdAt: Math.floor(new Date())});
         }catch (err){
             socket.emit(data.key.split("-")[1], {error: err});

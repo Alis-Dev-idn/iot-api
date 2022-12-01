e5e5dfcac3fe9fa8f9fd7bdaa9f2929a97a195fa
Alis-Dev-idn <ali.dev313@gmail.com>
2022-12-01 08:23:16 +0700

    add expire data 7 day

--- src/mongodb/model/device.schema.js
+++ src/mongodb/model/device.schema.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 const DeviceSchema = new mongoose.Schema({
     key: {type: String, required: true},
-    data: {type: Object(), required: true}
+    data: {type: Object(), required: true},
+    expireAt: {
+        type: Date,
+        default: Date.now(),
+        index: {expires: '5m'}
+    }
 }, {timestamps: true});
 
-DeviceSchema.index({"expireAt": 1}, {expireAfterSeconds: 604800})
+// DeviceSchema.index({"expireAt": 1}, {expireAfterSeconds: 604800})
 
 export default DeviceSchema;
